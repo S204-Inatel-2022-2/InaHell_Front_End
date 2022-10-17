09dcabbd31a3a54d4f06528a97281858b2f5bad2
jvoliveirag <jv.oliveira@gmail.com>
2022-10-17 17:08:18 -0300

    Remoção de console.log

--- frontend/static/js/integration/getParams.js
+++ frontend/static/js/integration/getParams.js
@@ -6,7 +6,6 @@ const platforms = urlParams.get("platforms");
 const filme_ou_serie = urlParams.get("filme_ou_serie");
 
 //passar os parametros para consulta no banco e retorno do filme "sorteado"
-console.log(get_idade(idade));
 
 console.log(idade);
 console.log(genero_fav);
