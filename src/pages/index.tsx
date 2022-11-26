861ede9991ec97646a95710f192aa537706b0baf
jvoliveirag <jv.oliveira@gmail.com>
2022-11-26 17:13:32 -0300

    Ajuste no layout e na responsividade da página inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,30 +59,34 @@ export default function Home() {
   }
 
   return (
-    <div className="max-w-[1124px] h-screen mr-52 ml-72 grid grid-cols-2 items-center">
-      <main className="mt-16 mb-36">
-        <Image src={logoImg} alt="Logo" className="mt-6"/>
+    <div className="grid grid-cols-6 gap-4 items-center">
 
-        <h1 className="mt-8 mb-8 text-white text-4xl font-bold leading-tight">
+      <div className='col-start-3 col-end-5 mt-28 items-center'>
+        <Image 
+          src={logoImg} 
+          alt="Logo" 
+          quality={100}
+          width={800}
+          height={800}
+        />
+      </div>
+
+      <div className='col-start-2 col-end-6 mr-14 ml-14 mt-8'>
+        <h1 className="mb-8 ml-24 mr-24 text-white text-center text-4xl font-bold leading-tight">
           Insira suas preferências e te indicaremos algo legal para assistir!
         </h1>
+      </div>
 
+      <div className='col-start-3 col-end-5 mt-2'>
         <Link href="/age"><button 
-            className="bg-orange-500 px-6 py-4 rounded text-black font-extrabold text-3xl hover:bg-orange-700"
-            type="submit"
-          >
-            Vamos lá!
+              className="animate-pulse w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-orange-500 rounded-lg focus:shadow-outline hover:bg-indigo-800 "
+              type="submit"
+            >
+              Começar
           </button>
         </Link>
-      </main>
+      </div>
 
-      <Image 
-        src={appPreviewImg} 
-        alt="" 
-        quality={100}
-        width={500}
-        height={500}
-      />
     </div>
   )
 }
